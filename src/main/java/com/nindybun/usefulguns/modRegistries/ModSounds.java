25ad64eb7dad81340efee8b22d7ed41ca4e360a3
NindyBun <andyco5665@gmail.com>
2023-03-06 05:47:00 -0800

    working on sounds

--- src/main/java/com/nindybun/usefulguns/modRegistries/ModSounds.java
+++ src/main/java/com/nindybun/usefulguns/modRegistries/ModSounds.java
@@ -12,10 +12,12 @@ import net.minecraftforge.registries.IForgeRegistry;
 public class ModSounds {
     public static final DeferredRegister<SoundEvent> SOUNDS = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, UsefulGuns.MOD_ID);
 
+    public static final RegistryObject<SoundEvent> GUNNER = SOUNDS.register("gunner.shoot", () -> new SoundEvent(new ResourceLocation(UsefulGuns.MOD_ID, "gunner.shoot")));
+    public static final RegistryObject<SoundEvent> SHOTGUN = SOUNDS.register("shotgun.shoot", () -> new SoundEvent(new ResourceLocation(UsefulGuns.MOD_ID, "shotgun.shoot")));
+    public static final RegistryObject<SoundEvent> RIFLE = SOUNDS.register("rifle.shoot", () -> new SoundEvent(new ResourceLocation(UsefulGuns.MOD_ID, "rifle.shoot")));
     public static final RegistryObject<SoundEvent> SNIPER = SOUNDS.register("sniper.shoot", () -> new SoundEvent(new ResourceLocation(UsefulGuns.MOD_ID, "sniper.shoot")));
-
     public static final RegistryObject<SoundEvent> PISTOL = SOUNDS.register("pistol.shoot", () -> new SoundEvent(new ResourceLocation(UsefulGuns.MOD_ID, "pistol.shoot")));
-    public static final RegistryObject<SoundEvent> DRY_FIRED = SOUNDS.register("pistol.dry", () -> new SoundEvent(new ResourceLocation(UsefulGuns.MOD_ID, "pistol.dry")));
 
+    public static final RegistryObject<SoundEvent> DRY_FIRED = SOUNDS.register("pistol.dry", () -> new SoundEvent(new ResourceLocation(UsefulGuns.MOD_ID, "pistol.dry")));
 
 }
